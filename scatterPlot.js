88826d0e81d4943109df834a18f10a4e78e60474
Caleb T. Vanorio <caleb.vanorio@centre.edu>
2019-03-10 16:42:52 -0400

    maybe done

--- scatterPlot.js
+++ scatterPlot.js
@@ -31,18 +31,20 @@ var drawScatter = function(width,height,data)
 {
 
   var margins ={
-    top:height/20,
-    left:width/32,
-    bottom:height/10,
-    right:width/10
+    top:50,
+    left:50,
+    bottom:50,
+    right:80
   };
 
-  width = width-margins.left-margins.right;
-  height = height-margins.top-margins.bottom;
-  var colors = d3.scaleOrdinal(d3.schemeAccent);
+  width = width+margins.left+margins.right;
+  height = height+margins.top+margins.bottom;
+
+
+  var colors = d3.scaleOrdinal(d3.schemeSet2);
   var xScale = d3.scaleLinear()
                 .domain([0,20])
-                .range([0,width]);
+                .range([0,width-margins.left-margins.right]);
   var yScale = d3.scaleLinear()
                 .domain([0,100])
                 .range([height,0]);
@@ -51,12 +53,11 @@ var drawScatter = function(width,height,data)
   var svg = d3.select("body")
       .append("svg")
       .attr("width",width)
-      .attr("height",height)
-      .style("border","1px solid black");
+      .attr("height",height);
 
   var plot = svg.append("g")
             .classed("plot",true)
-            .attr("transform","translate("+margins.left+","+margins.top+")");
+            .attr("transform","translate("+(.1*width)+","+(.1 *height)+")"+" scale(.8,.8)");
 
 
   var students = plot.selectAll("g")
@@ -73,17 +74,21 @@ var drawScatter = function(width,height,data)
           .attr("cy",function(d){return yScale(d);})
           .attr("r",function(){return .01*width;});
 
+
+
+
   var legend = svg.append("g")
             .classed("legend",true)
             .attr("transform",function(d){
               console.log(width-margins.right);
-              return "translate("+(width-margins.right)+",0)";});
+              return "translate("+(20+width-margins.right)+","+(.5*height)+")";});
+
   var legendLines = legend.selectAll("g")
           .data(data)
           .enter()
           .append("g")
           .classed("legendLine",true)
-          .attr("transform",function(d,i){return "translate(0,"+(i*20)+")";});
+          .attr("transform",function(d,i){return "translate(0,"+((i*20))+")";});
 
 
   legendLines.append("rect")
@@ -98,9 +103,20 @@ var drawScatter = function(width,height,data)
             .attr("x",0)
             .attr("y",0)
             .text(function(d,i){return d.name})
-            .attr("transform","translate(15,20)");
+            .attr("transform","translate(15,10)");
 
+    var xAxis = d3.axisBottom(xScale)
+    .tickValues([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]);
 
+    svg.append("g").classed("xAxis",true)
+        .call(xAxis)
+        .attr("transform","translate("+(.1*width)+","+(height-.7*margins.top)+")"+" scale(.8,.8)");;
+    var yAxis = d3.axisLeft(yScale);
+    svg.append("g").classed("yAxis",true)
+        .call(yAxis)
+        .attr("transform","translate("+(.1*width-.3*margins.left)+","+(.1 *height)+")"+" scale(.8,.8)");;
+
+  d3.select("body").append("br");
 
 
 
